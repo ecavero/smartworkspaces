67d6fde04af46e019ba22f1d48ad024488b1d911
Eduardo Cavero <ecavero@gmail.com>
2025-12-04 12:59:58 -0500

    Primer commit.

---
+++ src/main/java/pe/isil/smartworkspaces/SmartworkspacesApplication.java
@@ -0,0 +1,13 @@
+package pe.isil.smartworkspaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartworkspacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartworkspacesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/isil/smartworkspaces/SmartworkspacesApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.isil.smartworkspaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartworkspacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
