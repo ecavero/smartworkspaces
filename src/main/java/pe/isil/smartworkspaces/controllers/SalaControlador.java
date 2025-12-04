eeff8c11806b1eebc8a45817fdf92d0f0938f53b
Eduardo Cavero <ecavero@gmail.com>
2025-12-04 18:01:02 -0500

    Implementar Editar Sala.

--- src/main/java/pe/isil/smartworkspaces/controllers/SalaControlador.java
+++ src/main/java/pe/isil/smartworkspaces/controllers/SalaControlador.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -37,5 +38,12 @@ public class SalaControlador {
    public String guardarSala(Model model, Sala sala) {
       salaRepositorio.save(sala);
       return "redirect:/admin/salas/";
+   }
+
+   @GetMapping("/editar/{id}")
+   public String editarSala(@PathVariable Integer id, Model model) {
+      Sala sala = salaRepositorio.findById(id).orElseThrow(() -> new RuntimeException("Sala no encontrada"));
+      model.addAttribute("sala", sala);
+      return "admin/editar-sala";
    }
 }
