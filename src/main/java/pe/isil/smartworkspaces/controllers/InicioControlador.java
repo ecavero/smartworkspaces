02d9e28c5b8ff9f840fd0e129a751eb0a29b345f
Eduardo Cavero <ecavero@gmail.com>
2025-12-09 15:21:29 -0500

    Corrección a pantalla de inicio.

--- src/main/java/pe/isil/smartworkspaces/controllers/InicioControlador.java
+++ src/main/java/pe/isil/smartworkspaces/controllers/InicioControlador.java
@@ -1,13 +1,26 @@
 package pe.isil.smartworkspaces.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pe.isil.smartworkspaces.models.Sala;
+import pe.isil.smartworkspaces.repositories.SalaRepositorio;
 
 @Controller
 @RequestMapping("/")
 public class InicioControlador {
+   
+   @Autowired
+   SalaRepositorio salaRepositorio;
+   @GetMapping("/")
    String index(Model model) {
+      List<Sala> salas = salaRepositorio.findByEstado(true);
+      model.addAttribute("salas", salas);
       return "index";
    }
 }
